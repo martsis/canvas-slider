4c663135bfd8b44eed7d2db69141e11422510793
Alexander Martsis <martsis@outlook.com>
2020-01-17 14:13:54 +0300

    Fix export

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
         path: path.resolve(__dirname, 'dist'),
         filename: 'canvas-slider.js'
     },
-    mode: 'development',
+    mode: 'production',
     module: {
         rules: [
             {
